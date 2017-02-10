53ca5a3ed5829751d5d615d2758147aec6e4536b
lhkze <550473417@qq.com>
2017-02-10 16:45:07 +0800

    update

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 # coding=utf-8
 import os
-
+from logging import StreamHandler
 
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "mysql+pymysql://root:root@localhost/test1"
 UPLOAD_FOLDER = '/tmp/permdir'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+file_handler = StreamHandler()
+file_handler.setLevel(logging.WARNING)
+app.logger.addHandler(file_handler)
