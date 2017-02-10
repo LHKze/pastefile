e3440d13bc51f54827ab28352832a1035d89782b
lhkze <550473417@qq.com>
2017-02-10 16:05:02 +0800

    update

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "mysql+pymysql://root:root@localhost/test1"
 UPLOAD_FOLDER = '/tmp/permdir'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
