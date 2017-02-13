ea998953e2d3cf8eef2e92b7070a3a02b80cd576
lhkze <550473417@qq.com>
2017-02-13 08:58:12 +0800

    update

--- config.py
+++ config.py
@@ -11,7 +11,8 @@ import os
 
 class HerokuConfig:
 	DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "mysql+pymysql://root:root@localhost/test1"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+	    'mysql+pymysql://root:root@localhost/test1'
     UPLOAD_FOLDER = '/tmp/permdir'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
